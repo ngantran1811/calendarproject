6e896529e031bf6cdb011885acae2315b0452f71
Le Bao Ngan Tran <ngantran1811@gmail.com>
2026-06-27 00:26:32 +0300

    update date info

--- firstproject/tracker.js
+++ firstproject/tracker.js
@@ -2,4 +2,22 @@
 var date = new Date();
 console.log(date);
 
+/* EXTRACT THE CURRENT DATE */
+var currentDate = date.getDate();
+var currentMonth = date.getMonth();
+var currentYear = date.getFullYear();
+var currentDay = date.getDay();
+
+console.log(currentDate);
+console.log(currentMonth);
+console.log(currentYear);
+console.log(currentDay);
+
+/* DATE INFO */
+var months = [
+  "January", "February", "March", "April", 
+  "May", "June", "July", "August", 
+  "September", "October", "November", "December"
+];
+
 
